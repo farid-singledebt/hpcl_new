1760e0ddf9a4bedc6e9bc7344006932e17258fc5
Farid Ansari <farid.ansari@singledebt.in>
2024-10-14 16:09:41 +0530

    pragati prangan media images

--- data/pragati.js
+++ data/pragati.js
@@ -4,28 +4,28 @@ const pragatiGalleryData = [
     body: "The stall, themed 'Transition and Sustainability,' focused on showcasing the company's role in facilitating sustainable energy solutions while navigating the evolving dynamics of the LPG business. Interactive displays highlighted key products offerings, digital solutions, and notable milestones in  LPG SBU's journey like the LPG Cavern, offering visitors an immersive and informative experience.",
     images: [],
     displayImage:
-      "https://hpcl.parmarketing.agency/images/hpcl/pragati/petrochemicals/1.webp",
+      "https://hpcl.parmarketing.agency/images/hpcl/pragati/lpg/1.webp",
   },
   {
     title: "Retail SBU",
     body: "The stall, themed 'Retail Transformation,' is designed to reflect HPCL's forward-thinking approach to revolutionizing the retail experience. With modern design elements and interactive displays, it showcases the latest advancements in fuel technology, loyalty programs, and digital payment solutions. The stall highlights our commitment to staying ahead of market trends and delivering seamless, technology-driven services to millions of consumers every day.",
     images: [],
     displayImage:
-      "https://hpcl.parmarketing.agency/images/hpcl/pragati/petrochemicals/1.webp",
+      "https://hpcl.parmarketing.agency/images/hpcl/pragati/retail_sbu/1.webp",
   },
   {
     title: "SOD SBU",
     body: "Pipelines SBU demonstrated the following through Roto-scopic Display (a moving screen). 1.	The journey of Pipelines since 1985 (starting with Mumbai-Pune Pipeline) to till date 2.	New Pipeline Projects envisaged till 2030 & Sustainable journey. A screen has been set-up displaying video gallery on various innovations/initiatives which are implemented 1st in the industry by Pipelines SBU and a Video on various challenging Pipeline Projects implemented by Pipelines SBU. Further, Smart Pump Station (1st in the industry) at Kalakada Station in Vijayawada Dharmapuri Pipeline was inaugurated by CMD in the presence of CFD. Smart pump stations are equipped with advanced instrumentation, enabling remote monitoring, control, and operation from non-hardship locations. These stations are designed with self-sufficient systems that require minimal human intervention from remote control centers, significantly enhancing safety in hazardous areas. The integration of smart technology allows for equipment management to be handled remotely, reducing on-site personnel requirements. Interactive video feeds provide real-time monitoring of equipment, ensuring operational integrity and supporting informed decision-making.",
     images: [],
     displayImage:
-      "https://hpcl.parmarketing.agency/images/hpcl/pragati/petrochemicals/1.webp",
+      "https://hpcl.parmarketing.agency/images/hpcl/pragati/sod_sbu/1.webp",
   },
   {
     title: "Engineering project and pipeline",
     body: "The SBU's stall was themed “Green Future”, suitably aligned with its sustainability and environmentally friendly initiatives. Inside the stall, many innovative displays were featured, such as a “Net Zero Terminal”, highlighting our commitment to reducing carbon footprints. For showcasing the use of cleaner energy for cooking, Ethanol Stove was displayed, and E Flex Fuel Kit was displayed for demonstrating solutions for flexible fuel usage. These displays were complemented with AV presentations, so as to provide to visitors, deeper insights  into these technologies and immersive experience about their impact on the environment.",
     images: [],
     displayImage:
-      "https://hpcl.parmarketing.agency/images/hpcl/pragati/petrochemicals/1.webp",
+      "https://hpcl.parmarketing.agency/images/hpcl/pragati/engineering/1.webp",
   },
   {
     title: "Petrochemicals",
@@ -39,50 +39,50 @@ const pragatiGalleryData = [
     body: "The theme 'Sustainability and Futuristic Technology,' was designed to showcase SBU's commitment to environmentally responsible products and advanced technological solutions. The stall featured a unique and eco-friendly design made from bamboo wood, providing a visually distinct space that reflected the SBU's focus on sustainability. Displays highlighted key aspects of Lubes and Aviation SBUs, including product packs, base oils, models and international market ventures. ",
     images: [],
     displayImage:
-      "https://hpcl.parmarketing.agency/images/hpcl/pragati/petrochemicals/1.webp",
+      "https://hpcl.parmarketing.agency/images/hpcl/pragati/lubes/1.webp",
   },
   {
     title: "Natural Gas and Renewables",
     body: "The stall, themed 'Fuelling Future,' was designed to highlight the company's commitment to clean and green energy solutions. This stall was shared between the Natural Gas and Biofuel & Renewable teams, showcasing our diverse portfolio in these sectors. LED screens showcased various projects in natural gas, biofuel, and renewable energy. The design also incorporated cutouts of renewable energy sources like windmills, solar panels, and CBG/CNG dispensing units, creating an immersive visual experience. The miniature model of the CGD Network, along with an interactive screen displayed details of key projects. An exciting feature was the launch of 'Organic Manure' with an AV explaining its benefits and future prospects. ",
     images: [],
     displayImage:
-      "https://hpcl.parmarketing.agency/images/hpcl/pragati/petrochemicals/1.webp",
+      "https://hpcl.parmarketing.agency/images/hpcl/pragati/natural_gas/1.webp",
   },
 ];
 
-for (let i = 1; i <= 4; i++) {
+for (let i = 1; i <= 2; i++) {
   pragatiGalleryData[0].images.push({
-    img: `https://hpcl.parmarketing.agency/images/hpcl/pragati/petrochemicals/${i}.webp`,
+    img: `https://hpcl.parmarketing.agency/images/hpcl/pragati/lpg/${i}.webp`,
+  });
+}
+for (let i = 1; i <= 8; i++) {
+  pragatiGalleryData[1].images.push({
+    img: `https://hpcl.parmarketing.agency/images/hpcl/pragati/retail_sbu/${i}.webp`,
+  });
+}
+for (let i = 1; i <= 6; i++) {
+  pragatiGalleryData[2].images.push({
+    img: `https://hpcl.parmarketing.agency/images/hpcl/pragati/sod_sbu/${i}.webp`,
+  });
+}
+for (let i = 1; i <= 5; i++) {
+  pragatiGalleryData[3].images.push({
+    img: `https://hpcl.parmarketing.agency/images/hpcl/pragati/engineering/${i}.webp`,
   });
 }
 for (let i = 1; i <= 4; i++) {
-  pragatiGalleryData[1].images.push({
+  pragatiGalleryData[4].images.push({
     img: `https://hpcl.parmarketing.agency/images/hpcl/pragati/petrochemicals/${i}.webp`,
-  });
-}
-for (let i = 1; i <= 9; i++) {
-  pragatiGalleryData[2].images.push({
-    img: `https://hpcl.parmarketing.agency/images/hpcl/pragati/natural_gas/${i}.webp`,
-  });
-}
-for (let i = 1; i <= 9; i++) {
-  pragatiGalleryData[3].images.push({
-    img: `https://hpcl.parmarketing.agency/images/hpcl/pragati/natural_gas/${i}.webp`,
   });
 }
 for (let i = 1; i <= 10; i++) {
-  pragatiGalleryData[4].images.push({
+  pragatiGalleryData[5].images.push({
     img: `https://hpcl.parmarketing.agency/images/hpcl/pragati/lubes/${i}.webp`,
   });
 }
-for (let i = 1; i <= 4; i++) {
-  pragatiGalleryData[5].images.push({
-    img: `https://hpcl.parmarketing.agency/images/hpcl/pragati/petrochemicals/${i}.webp`,
-  });
-}
-for (let i = 1; i <= 4; i++) {
+for (let i = 1; i <= 9; i++) {
   pragatiGalleryData[6].images.push({
-    img: `https://hpcl.parmarketing.agency/images/hpcl/pragati/petrochemicals/${i}.webp`,
+    img: `https://hpcl.parmarketing.agency/images/hpcl/pragati/natural_gas/${i}.webp`,
   });
 }
 
