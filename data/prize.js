f934beeaf7c49da85f4aae3f89a611681bd2581d
Farid Ansari <farid.ansari@singledebt.in>
2024-10-15 12:01:02 +0530

    thumbnail added in winner section

--- data/prize.js
+++ data/prize.js
@@ -3,7 +3,9 @@ $(document).ready(function () {
     item.addEventListener("click", function () {
       const imgTag = item.querySelector("img");
       const title = item.querySelector("h2");
-      document.getElementById("modal-img-div").setAttribute("src", imgTag.src);
+      document
+        .getElementById("modal-img-div")
+        .setAttribute("src", imgTag.getAttribute("data-img"));
       document.getElementById("image-title").innerHTML = title.innerHTML;
     });
   });
